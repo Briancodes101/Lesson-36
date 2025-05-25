209df907054b969b822b8e5fc050dadc4efbb133
Briancodes101 <ukiuwabrian@gmail.com>
2025-05-25 18:23:07 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#print statement
+
+print("Welcome to my Python World!")
+
+#printing number which is stored as a string
+
+print("2024")
+
+#printing number which is stored as a integer
+
+print(56)
+
+#next line
+
+print("\n")
+print("My name is Brian")
+
+#importing keywords
+
+import keyword
+print("\n")
+print("below are the keywords :")
+print(keyword.kwlist)
